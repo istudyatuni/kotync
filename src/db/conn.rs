ba2e6630994cce4470556252d606fa60968b14a7
Ilia <istudyatuni@gmail.com>
2024-04-30 15:18:58 +0300

    use transaction when sync favorites

--- src/db/conn.rs
+++ src/db/conn.rs
@@ -15,6 +15,7 @@ use crate::models::{
 const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 type ConnManager = ConnectionManager<DbConnection>;
+type Conn = PooledConnection<ConnManager>;
 
 pub struct DB {
     conn: Pool<ConnManager>,
@@ -72,14 +73,22 @@ impl DB {
         Ok(())
     }
     pub fn add_favourites_package(&self, pkg: &FavouritesPackage, user_id: UserID) -> Result<()> {
-        for c in &pkg.categories {
-            self.add_category(c.to_db(user_id))?;
-        }
-        for f in &pkg.favourites {
-            self.add_manga(f.manga.to_db())?;
-            self.add_tags(f.manga.tags.iter().map(|t| t.to_db()).collect(), f.manga_id)?;
-            self.add_favourite(f.to_db(user_id))?;
-        }
+        let conn = &mut self.pool()?;
+        conn.transaction::<_, anyhow::Error, _>(|conn| {
+            for c in &pkg.categories {
+                Self::add_category(conn, c.to_db(user_id))?;
+            }
+            for f in &pkg.favourites {
+                Self::add_manga(conn, f.manga.to_db())?;
+                Self::add_tags(
+                    conn,
+                    f.manga.tags.iter().map(|t| t.to_db()).collect(),
+                    f.manga_id,
+                )?;
+                Self::add_favourite(conn, f.to_db(user_id))?;
+            }
+            Ok(())
+        })?;
 
         Ok(())
     }
@@ -114,12 +123,12 @@ impl DB {
             .select((Favourite::as_select(), Manga::as_select()))
             .load(&mut self.pool()?)?)
     }
-    fn add_category(&self, category: Category) -> Result<()> {
+    fn add_category(conn: &mut Conn, category: Category) -> Result<()> {
         use super::schema::categories::dsl::categories;
 
         diesel::replace_into(categories)
             .values(vec![category])
-            .execute(&mut self.pool()?)?;
+            .execute(conn)?;
 
         Ok(())
     }
@@ -131,29 +140,27 @@ impl DB {
             .select(Category::as_select())
             .get_results(&mut self.pool()?)?)
     }
-    fn add_manga(&self, manga: Manga) -> Result<()> {
+    fn add_manga(conn: &mut Conn, manga: Manga) -> Result<()> {
         use super::schema::manga::dsl::manga as manga_table;
 
         diesel::replace_into(manga_table)
             .values(vec![manga])
-            .execute(&mut self.pool()?)?;
+            .execute(conn)?;
 
         Ok(())
     }
-    fn add_favourite(&self, favourite: Favourite) -> Result<()> {
+    fn add_favourite(conn: &mut Conn, favourite: Favourite) -> Result<()> {
         use super::schema::favourites::dsl::favourites;
 
         diesel::replace_into(favourites)
             .values(vec![favourite])
-            .execute(&mut self.pool()?)?;
+            .execute(conn)?;
 
         Ok(())
     }
-    fn add_tags(&self, tags: Vec<Tag>, manga_id: i64) -> Result<()> {
+    fn add_tags(conn: &mut Conn, tags: Vec<Tag>, manga_id: i64) -> Result<()> {
         use super::schema::manga_tags::dsl::manga_tags;
         use super::schema::tags::dsl::tags as tags_table;
-
-        let conn = &mut self.pool()?;
 
         diesel::replace_into(tags_table)
             .values(&tags)
@@ -182,7 +189,7 @@ impl DB {
             .select(Tag::as_select())
             .load(conn)?)
     }
-    fn pool(&self) -> Result<PooledConnection<ConnectionManager<DbConnection>>> {
+    fn pool(&self) -> Result<Conn> {
         self.conn.get().context("cannot get db.pool")
     }
 }
