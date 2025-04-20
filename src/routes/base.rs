4830fc6f6aa387f9f17702f5021b766cecd9a3a4
Ilia <istudyatuni@gmail.com>
2025-04-20 18:51:45 +0300

    fix fmt

--- src/routes/base.rs
+++ src/routes/base.rs
@@ -37,7 +37,7 @@ pub fn auth(
     let user = match user {
         Some(u) if req.check_password(&u).is_err() => {
             // todo: wait for 2s (configurable)
-            return Err((Status::BadRequest, "Wrong password").into())
+            return Err((Status::BadRequest, "Wrong password").into());
         }
         #[cfg(feature = "migrate-md5")]
         Some(u) if u.password.len() == MD5_LEN => {
